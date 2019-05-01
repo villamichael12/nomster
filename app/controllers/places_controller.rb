7f5166c11904b28850dba19b155af5fa9c021882
villamichael12 <villamichael13@yahoo.com>
2019-05-01 01:09:15 +0000

    added devise gems and set it up

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 
 		def index
-			@places = Place.all
+			#@places = Place.all
+			@places = Place.order("name").page(params[:page]).per_page(1)
 		end
 
 		def new 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,12 @@
-Rails.application.configure do
+Rails.application.configure do\
+   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.reload_classes_only_on_change = false
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -15,14 +17,14 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = false
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
 
     config.cache_store = :null_store
   end
