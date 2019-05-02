e1b2cc254369619fb1581889438de4070dccf868
villamichael12 <villamichael13@yahoo.com>
2019-05-02 09:49:32 +0000

    added restrictions to delete function

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 		def index
 			#@places = Place.all
@@ -21,16 +21,28 @@ class PlacesController < ApplicationController
 
   	def edit
   		@place = Place.find(params[:id]) 
+
+  		if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
   	end
 
   	def update
   		@place = Place.find(params[:id])
+  		if @place.user != current_user
+    		return render plain: 'Not Allowed', status: :forbidden
+  		end
+
   		@place.update_attributes(place_params)
   		redirect_to root_path
   	end
 
   	def destroy
   	@place = Place.find(params[:id]) 
+  	if @place.user != current_user
+    	return render plain: 'Not Allowed', status: :forbidden
+  	end
+
   	@place.destroy
   	redirect_to root_path
   	end
