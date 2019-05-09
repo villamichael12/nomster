6766d65d260cbc4ce2a37427b0e0c6cc16dde3fd
villamichael12 <villamichael13@yahoo.com>
2019-05-09 10:05:38 +0000

    created model for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
 	belongs_to :user
+	has_many :comments
+
 	geocoded_by :address
   after_validation :geocode
-  
+
 	validates :name, :address, :description, presence: true
 	validates :name, length: {minimum: 3} 
 end
